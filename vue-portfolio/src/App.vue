d813ad210f0090c5aa6ecbd9a920f80a0bdbcd39
Mitchel Severe <truth531@gmail.com>
2017-10-16 17:47:55 -0400

    add navigation component

--- vue-portfolio/src/App.vue
+++ vue-portfolio/src/App.vue
@@ -1,14 +1,19 @@
 <template>
   <div id="app">
+    <navigation></navigation>
     <router-view/>
   </div>
 </template>
 
 <script>
 import './assets/scss/main.scss';
+import Navigation from './components/Navigation';
 
 export default {
   name: 'app',
+  components: {
+    Navigation,
+  },
 };
 </script>
 
---
+++ vue-portfolio/src/components/Navigation.vue
@@ -0,0 +1,58 @@
+<template>
+  <nav ref="navbar">
+    <div class="logo">
+      <router-link to="/">{{ logo }}</router-link>
+    </div>
+    <div class="links">
+      <router-link to="/github"><i class="fa fa-github" aria-hidden="true"></i></router-link>
+      <router-link to="/behance"><i class="fa fa-behance" aria-hidden="true"></i></router-link>
+      <router-link to="/linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></router-link>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+  name: 'Navigation',
+  data() {
+    return {
+      logo: 'Mitchel Severe',
+    };
+  },
+  mounted() {
+    console.log(this.$refs.navbar.clientTop);
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped>
+$main-color: #0D0D0D;
+  nav {
+    background: $main-color;
+    width: 100%;
+    height: 10vh;
+    position: fixed;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    padding: 0px 25px;
+  }
+  .logo {
+    font-size: 20px;
+    letter-spacing: 1px;
+    a {
+      color: #FFF;
+    }
+  }
+  .links {
+    margin-right: 40px;
+    a {
+      color: #FFF;
+    }
+    i {
+      font-size: 20px;
+      margin: 0px 5px;
+    }
+  }
+</style>
