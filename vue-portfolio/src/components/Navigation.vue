948248cab84bcc990892689215d9271c6eaa2f94
Mitchel Severe <truth531@gmail.com>
2017-10-17 09:11:01 -0400

    add scroll method and styling

--- vue-portfolio/src/components/Navigation.vue
+++ vue-portfolio/src/components/Navigation.vue
@@ -1,7 +1,7 @@
 <template>
-  <nav ref="navbar">
+  <nav :class="[scrollPosition > 720 ? 'scroll-nav' : 'top-nav']">
     <div class="logo">
-      <router-link to="/">{{ logo }}</router-link>
+      <span><router-link to="/">{{ logo }}</router-link></span>
     </div>
     <div class="links">
       <router-link to="/github"><i class="fa fa-github" aria-hidden="true"></i></router-link>
@@ -16,43 +16,82 @@ export default {
   name: 'Navigation',
   data() {
     return {
-      logo: 'Mitchel Severe',
+      logo: 'M',
+      scrollPosition: 0,
     };
   },
   mounted() {
-    console.log(this.$refs.navbar.clientTop);
+    window.addEventListener('scroll', this.updateScroll);
+  },
+  methods: {
+    updateScroll() {
+      this.scrollPosition = window.scrollY;
+      console.log(this.scrollPosition);
+    },
   },
 };
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
 $main-color: #0D0D0D;
+$secondary-color: #FFF;
+$accent-color: #4C4C4C;
+$font-stack-logo: 'Monoton', cursive;
   nav {
-    background: $main-color;
     width: 100%;
     height: 10vh;
     position: fixed;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: 0px 25px;
+    padding: 0px 45px;
+    transition: 300ms;
   }
+
   .logo {
-    font-size: 20px;
-    letter-spacing: 1px;
+    font-size: 50px;
     a {
-      color: #FFF;
+      letter-spacing: 0.2px;
+      font-family: $font-stack-logo;
     }
   }
+
   .links {
-    margin-right: 40px;
-    a {
-      color: #FFF;
-    }
+    margin-right: 70px;
+    margin-top: 10px;
     i {
-      font-size: 20px;
-      margin: 0px 5px;
+      font-size: 25px;
+      margin: 0px 8px;
+    }
+  }
+
+  .scroll-nav {
+    background-color: $secondary-color;
+    border-bottom: 1px solid $accent-color;
+    a {
+      color: $main-color;
+    }
+    span {
+      transition: 50ms;
+        &:hover {
+          font-family: Arial !important;
+        }
+    }
+  }
+
+  .top-nav {
+    background-color: $main-color;
+    a {
+      color: $secondary-color;
+    }
+    span a {
+      color: $secondary-color;
+      margin-bottom: 3px;
+      border-bottom: 0px solid $secondary-color;
+      transition: 200ms ease-in-out;
+      &:hover {
+        border-bottom: 5px solid $secondary-color;
+      }
     }
   }
 </style>
