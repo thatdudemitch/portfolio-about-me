ae2f078650c83e9748349244c120071ee9bbf376
Mitchel Severe <truth531@gmail.com>
2017-10-17 15:58:14 -0400

    add link, hover and nav styles

--- vue-portfolio/src/components/Navigation.vue
+++ vue-portfolio/src/components/Navigation.vue
@@ -3,10 +3,8 @@
     <div class="logo">
       <span><router-link to="/">{{ logo }}</router-link></span>
     </div>
-    <div class="links">
-      <router-link to="/github"><i class="fa fa-github" aria-hidden="true"></i></router-link>
-      <router-link to="/behance"><i class="fa fa-behance" aria-hidden="true"></i></router-link>
-      <router-link to="/linkedin"><i class="fa fa-linkedin" aria-hidden="true"></i></router-link>
+    <div class="link">
+      <router-link to="mitchel-severe-resume">View Resume</router-link>
     </div>
   </nav>
 </template>
@@ -26,7 +24,6 @@ export default {
   methods: {
     updateScroll() {
       this.scrollPosition = window.scrollY;
-      console.log(this.scrollPosition);
     },
   },
 };
@@ -35,8 +32,11 @@ export default {
 <style lang="scss" scoped>
 $main-color: #0D0D0D;
 $secondary-color: #FFF;
-$accent-color: #4C4C4C;
+$accent-color: #7C7C7C;
+
 $font-stack-logo: 'Monoton', cursive;
+$font-stack-tertiary: 'Helvetica', sans-serif;
+
   nav {
     width: 100%;
     height: 10vh;
@@ -44,38 +44,50 @@ $font-stack-logo: 'Monoton', cursive;
     display: flex;
     justify-content: space-between;
     align-items: center;
-    padding: 0px 45px;
     transition: 300ms;
   }
 
   .logo {
-    font-size: 50px;
+    font-size: 5vh;
+    margin-left: 30px;
     a {
-      letter-spacing: 0.2px;
       font-family: $font-stack-logo;
     }
   }
 
-  .links {
-    margin-right: 70px;
-    margin-top: 10px;
-    i {
-      font-size: 25px;
-      margin: 0px 8px;
+  .link {
+    margin-right: 30px;
+    font-family: 'Helvetica', sans-serif;
+    font-weight: lighter;
+    a {
+      height: 25px;
+      transition: 100ms ease-in-out;
+      margin-bottom: 4px;
     }
   }
 
   .scroll-nav {
     background-color: $secondary-color;
-    border-bottom: 1px solid $accent-color;
+    box-shadow: 0 5px 5px -6px $accent-color;
     a {
       color: $main-color;
     }
     span {
-      transition: 50ms;
+      a {
+        color: $main-color;
+        transition: 500ms ease-in-out;
         &:hover {
-          font-family: Arial !important;
+          transform: rotate(180deg);
+          font-size: 6vh;
         }
+      }
+    } 
+    .link {
+      a {
+        &:hover {
+          border-bottom: 1px solid $main-color;
+        }
+      }
     }
   }
 
@@ -86,11 +98,17 @@ $font-stack-logo: 'Monoton', cursive;
     }
     span a {
       color: $secondary-color;
-      margin-bottom: 3px;
-      border-bottom: 0px solid $secondary-color;
-      transition: 200ms ease-in-out;
+      transition: 500ms ease-in-out;
       &:hover {
-        border-bottom: 5px solid $secondary-color;
+        transform: rotate(180deg);
+        font-size: 6vh;
+      }
+    }
+    .link {
+      a {
+        &:hover {
+          border-bottom: 1px solid $secondary-color;
+        }
       }
     }
   }
