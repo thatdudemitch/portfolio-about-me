769800b61571c9d4b0385428cfdd50c6ac9f9464
Mitchel Severe <truth531@gmail.com>
2017-10-18 08:24:54 -0400

    add path routes

--- vue-portfolio/src/router/index.js
+++ vue-portfolio/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Index from '../components/Index';
+import Resume from '../components/Resume';
 
 Vue.use(Router);
 
@@ -8,8 +9,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Hello',
-      component: HelloWorld,
+      name: 'Index',
+      component: Index,
+    },
+    {
+      path: '/mitchel-severe-resume',
+      name: 'Resume',
+      component: Resume,
     },
   ],
 });
