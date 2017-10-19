94df3971c2be60a6eaaaa9c952450c4c06bbcb12
Mitchel Severe <truth531@gmail.com>
2017-10-19 12:29:32 -0400

    remove resume route

--- vue-portfolio/src/router/index.js
+++ vue-portfolio/src/router/index.js
@@ -1,7 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import Index from '../components/Index';
-import Resume from '../components/Resume';
 
 Vue.use(Router);
 
@@ -11,11 +10,6 @@ export default new Router({
       path: '/',
       name: 'Index',
       component: Index,
-    },
-    {
-      path: '/mitchel-severe-resume',
-      name: 'Resume',
-      component: Resume,
     },
   ],
 });
