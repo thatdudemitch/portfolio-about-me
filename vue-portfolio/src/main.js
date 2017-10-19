0a6d24205fef29ed4d10cde72d297e0e6a595fa8
Mitchel Severe <truth531@gmail.com>
2017-10-18 23:19:23 -0400

    add vue-scroll-reveal

--- vue-portfolio/src/main.js
+++ vue-portfolio/src/main.js
@@ -1,9 +1,11 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import VueScrollReveal from 'vue-scroll-reveal';
 import Vue from 'vue';
 import App from './App';
 import router from './router';
 
+Vue.use(VueScrollReveal);
 Vue.config.productionTip = false;
 
 /* eslint-disable no-new */
